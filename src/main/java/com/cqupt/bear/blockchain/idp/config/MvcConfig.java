6071d47866d9e5894dcfb28104999c3c4f86f3b6
ybeario <571523412@qq.com>
2018-12-07 14:49:52 +0800

    完成https配置并且上线

--- src/main/java/com/cqupt/bear/blockchain/idp/config/MvcConfig.java
+++ src/main/java/com/cqupt/bear/blockchain/idp/config/MvcConfig.java
@@ -19,8 +19,8 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addViewController("/success").setViewName("success");
 		registry.addViewController("/index").setViewName("index");
 		registry.addViewController("/register").setViewName("register");
-		registry.addViewController("/admin").setViewName("admin/index");
-		registry.addViewController("/officer").setViewName("officer/index");
-		registry.addViewController("/user").setViewName("user/index");
+		registry.addViewController("/admin").setViewName("admin/adminIndex");
+		registry.addViewController("/officer").setViewName("officer/officerIndex");
+		registry.addViewController("/user").setViewName("user/userIndex");
 	}
 }
